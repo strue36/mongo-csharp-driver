03968ee9f85bed9d0e1397ab1cceb68824eecae3
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2023-08-31 16:08:06 -0700

    WIP

--- evergreen/packages-push.sh
+++ evergreen/packages-push.sh
@@ -12,11 +12,6 @@ if [ -z "$PACKAGES_SOURCE_KEY" ]; then
   exit 1
 fi
 
-if [ -z "$PACKAGE_VERSION" ]; then
-  PACKAGE_VERSION=$(sh ./evergreen/packages-version.sh)
-  echo Calculated PACKAGE_VERSION value: "$PACKAGE_VERSION"
-fi
-
 dotnet nuget push --source "$PACKAGES_SOURCE" --api-key "$PACKAGES_SOURCE_KEY" ./build/nuget/MongoDB.Bson."$PACKAGE_VERSION".nupkg
 dotnet nuget push --source "$PACKAGES_SOURCE" --api-key "$PACKAGES_SOURCE_KEY" ./build/nuget/MongoDB.Driver.Core."$PACKAGE_VERSION".nupkg
 dotnet nuget push --source "$PACKAGES_SOURCE" --api-key "$PACKAGES_SOURCE_KEY" ./build/nuget/MongoDB.Driver."$PACKAGE_VERSION".nupkg
