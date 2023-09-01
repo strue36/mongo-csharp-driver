9a22aacda0112a84b8e54fed3069ccc1c62fe410
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2023-09-01 11:26:15 -0700

    PR

--- evergreen/packages-version.sh
+++ evergreen/packages-version.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -o errexit  # Exit the script with error if any of the commands fail
 
-if [ -z "$PACKAGE_VERSION_MATCH" ]; then
+if [ -z "$PACKAGE_TARGET" ]; then
   # Use production release tag if nothing was passed
+  PACKAGE_TARGET="release"
+fi
+
+if [ "$PACKAGE_TARGET" == "dev" ]; then
+  PACKAGE_VERSION_MATCH="v[0-9]*.[0-9]*.[0-9]*-dev[0-9]*"
+  PACKAGE_VERSION_EXCLUDE=""
+elif [ "$PACKAGE_TARGET" == "release" ]; then
   PACKAGE_VERSION_MATCH="v[0-9]*.[0-9]*.[0-9]*"
   PACKAGE_VERSION_EXCLUDE="*-dev[0-9]*"
+else
+  echo "Unexpected value of $PACKAGE_TARGET: ${PACKAGE_TARGET}"
 fi
 
 PACKAGE_VERSION=$(git describe --tags --abbrev=0 --match="${PACKAGE_VERSION_MATCH}" --exclude="${PACKAGE_VERSION_EXCLUDE}")
