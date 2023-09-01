4bd43641353baba6b3e86dc3b5f0f683525589d1
Oleksandr Poliakov <oleksandr.poliakov@mongodb.com>
2023-09-01 12:21:29 -0700

    wip

--- evergreen/packages-version.sh
+++ evergreen/packages-version.sh
@@ -6,14 +6,14 @@ if [ -z "$PACKAGE_TARGET" ]; then
   PACKAGE_TARGET="release"
 fi
 
-if [ "$PACKAGE_TARGET" == "dev" ]; then
+if [[ "$PACKAGE_TARGET" == "dev" ]]; then
   PACKAGE_VERSION_MATCH="v[0-9]*.[0-9]*.[0-9]*-dev[0-9]*"
   PACKAGE_VERSION_EXCLUDE=""
-elif [ "$PACKAGE_TARGET" == "release" ]; then
+elif [[ "$PACKAGE_TARGET" == "release" ]]; then
   PACKAGE_VERSION_MATCH="v[0-9]*.[0-9]*.[0-9]*"
   PACKAGE_VERSION_EXCLUDE="*-dev[0-9]*"
 else
-  echo "Unexpected value of $PACKAGE_TARGET: ${PACKAGE_TARGET}"
+  echo "Unexpected value of PACKAGE_TARGET: ${PACKAGE_TARGET}"
 fi
 
 PACKAGE_VERSION=$(git describe --tags --abbrev=0 --match="${PACKAGE_VERSION_MATCH}" --exclude="${PACKAGE_VERSION_EXCLUDE}")
